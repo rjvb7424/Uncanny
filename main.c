713dbe5932cce6d4169ffd5acb036b3383a2ddc5
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-07-01 10:45:09 +0100

    Refactor SDL initialization and rendering loop for improved functionality

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
@@ -23,10 +23,36 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    SDL_Delay(90000);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer)
+    {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_Event e;
+    int quit = 0;
+    Uint32 start = SDL_GetTicks();
+
+    while (!quit && SDL_GetTicks() - start < 3000)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+                quit = 1;
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
